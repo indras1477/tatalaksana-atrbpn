59206b7688d98eafc0834342debfdc5aaa39f5b8
indras1477 <indras1477@gmail.com>
2026-04-06 10:36:38 +0700

    feat: change favicon to icon-bpn.ico

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -15,6 +15,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "E-SOP ATR BPN",
   description: "Sistem Operasional Standard ATR BPN",
+  icons: {
+    icon: '/icon-bpn.ico',
+  },
 };
 
 export default function RootLayout({
